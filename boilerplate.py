51305c84f45c1d8300fd41a050a7d099bf9b5985
Sam Shelton <sam@shelt.ca>
2015-11-23 19:16:56 -0600

    Create boilerplate.py

---
+++ boilerplate.py
@@ -0,0 +1,39 @@
+import praw
+import requests
+
+# Account settings (private)
+USERNAME = ''
+PASSWORD = ''
+
+# OAuth settings (private)
+CLIENT_ID = ''
+CLIENT_SECRET = ''
+REDIRECT_URI = 'http://127.0.0.1:65010/authorize_callback'
+
+# Configuration Settings
+USER_AGENT = ""
+AUTH_TOKENS = ["identity","read"]
+
+def get_access_token():
+    response = requests.post("https://www.reddit.com/api/v1/access_token",
+      auth = requests.auth.HTTPBasicAuth(CLIENT_ID, CLIENT_SECRET),
+      data = {"grant_type": "password", "username": USERNAME, "password": PASSWORD},
+      headers = {"User-Agent": USER_AGENT})
+    return dict(response.json())["access_token"]
+
+def get_praw():
+    r = praw.Reddit(USER_AGENT)
+    r.set_oauth_app_info(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI)
+    r.set_access_credentials(set(AUTH_TOKENS), get_access_token())
+    return r
+
+def main(r):
+    try:
+        ##### MAIN CODE #####
+
+if __name__ == "__main__":
+    while True:
+        try:
+            main(get_praw())
+        except praw.errors.OAuthInvalidToken:
+            print("OAuth token expired.")
